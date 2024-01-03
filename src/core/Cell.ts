4855445785f41b2f8839a5ce4fcf9374a6fb2c44
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2024-01-03 14:00:05 +0100

    refactor: english translations

--- src/core/Cell.ts
+++ src/core/Cell.ts
@@ -26,7 +26,13 @@ class Cell {
 		this.status = status;
 	}
 
-	calculateStatusBasedOnNeighbors(neighbors: number): CellStatus {
+	/*
+	 * Any live cell with fewer than two live neighbours dies, as if caused by underpopulation
+	 * Any live cell with more than three live neighbours dies, as if by overcrowding
+	 * Any live cell with two or three live neighbours lives on the next generation
+	 * Any dead cell with exactly three live neighbours becomes a live cell
+	 */
+	calculateStatusBasedOnNeighbours(neighbors: number): CellStatus {
 		if (this.isAlive() && neighbors < 2) return CellStatus.dead;
 		if (this.isAlive() && neighbors > 3) return CellStatus.dead;
 		if (this.isAlive() && (neighbors === 2 || neighbors === 3)) return CellStatus.alive;
--- src/tests/cell.test.ts
+++ src/tests/cell.test.ts
@@ -23,23 +23,23 @@ describe('The Bowling Game', () => {
 		expect(cellDead.getStatus()).toBe(CellStatus.alive);
 	});
 
-	it('Cualquier célula viva con menos de dos vecinos vivos muere, por poca población', () => {
-		expect(cellAlive.calculateStatusBasedOnNeighbors(0)).toBe(CellStatus.dead);
-		expect(cellAlive.calculateStatusBasedOnNeighbors(1)).toBe(CellStatus.dead);
+	it('any live cell with fewer than two live neighbours dies, as if caused by underpopulation', () => {
+		expect(cellAlive.calculateStatusBasedOnNeighbours(0)).toBe(CellStatus.dead);
+		expect(cellAlive.calculateStatusBasedOnNeighbours(1)).toBe(CellStatus.dead);
 	});
 
-	it('Cualquier célula viva con mas de tres vecinos muere, por sobrepoblación', () => {
-		expect(cellAlive.calculateStatusBasedOnNeighbors(4)).toBe(CellStatus.dead);
-		expect(cellAlive.calculateStatusBasedOnNeighbors(5)).toBe(CellStatus.dead);
-		expect(cellAlive.calculateStatusBasedOnNeighbors(6)).toBe(CellStatus.dead);
+	it('any live cell with more than three live neighbours dies, as if by overcrowding', () => {
+		expect(cellAlive.calculateStatusBasedOnNeighbours(4)).toBe(CellStatus.dead);
+		expect(cellAlive.calculateStatusBasedOnNeighbours(5)).toBe(CellStatus.dead);
+		expect(cellAlive.calculateStatusBasedOnNeighbours(6)).toBe(CellStatus.dead);
 	});
 
-	it('Cualquier célula viva con dos o tres vecinos, vive', () => {
-		expect(cellAlive.calculateStatusBasedOnNeighbors(2)).toBe(CellStatus.alive);
-		expect(cellAlive.calculateStatusBasedOnNeighbors(3)).toBe(CellStatus.alive);
+	it('any live cell with two or three live neighbours lives on the next generation', () => {
+		expect(cellAlive.calculateStatusBasedOnNeighbours(2)).toBe(CellStatus.alive);
+		expect(cellAlive.calculateStatusBasedOnNeighbours(3)).toBe(CellStatus.alive);
 	});
 
-	it('Célula muerta con 3 vecinos, resucita', () => {
-		expect(cellDead.calculateStatusBasedOnNeighbors(3)).toBe(CellStatus.alive);
+	it('any dead cell with exactly three live neighbours becomes a live cell', () => {
+		expect(cellDead.calculateStatusBasedOnNeighbours(3)).toBe(CellStatus.alive);
 	});
 });
