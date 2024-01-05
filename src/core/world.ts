ca33874231a31d68d670f0b2defe7e1d2cfe58cd
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2024-01-05 20:25:02 +0100

    refactor: move World in a class

--- src/core/world.ts
+++ src/core/world.ts
@@ -1,9 +1,50 @@
-import { Cell } from './cell';
+import { Cell, CellStatus } from '../core/cell';
+
+const Alive = CellStatus.alive;
+// const Dead = CellStatus.dead;
 
 class World {
-	private cells: Cell[][];
+	private readonly lastYIndex: number;
+	private readonly lastXIndex: number;
 
-	getCells = () => this.cells;
+	constructor(private cellMatrix: Cell[][]) {
+		this.cellMatrix = cellMatrix;
+		this.lastYIndex = cellMatrix.length - 1;
+		this.lastXIndex = cellMatrix.length - 1;
+	}
+
+	getCellMatrix = () => this.cellMatrix;
+
+	calculateNeighboursFromCoors(x: number, y: number) {
+		let numberOfNeighbours = 0;
+
+		/**
+		 * [x-1 & y-1] [ x  & y-1] [x+1 & y-1]
+		 * [x-1 &  y ] [   x y   ] [x+1 &  y ]
+		 * [x-1 & y+1] [ x  & y+1] [x+1 & y+1]
+		 */
+		if (this.isAliveNeighboursByCoors(x - 1, y - 1)) numberOfNeighbours++;
+		if (this.isAliveNeighboursByCoors(x - 1, y)) numberOfNeighbours++;
+		if (this.isAliveNeighboursByCoors(x - 1, y + 1)) numberOfNeighbours++;
+
+		if (this.isAliveNeighboursByCoors(x, y - 1)) numberOfNeighbours++;
+		if (this.isAliveNeighboursByCoors(x, y + 1)) numberOfNeighbours++;
+
+		if (this.isAliveNeighboursByCoors(x + 1, y - 1)) numberOfNeighbours++;
+		if (this.isAliveNeighboursByCoors(x + 1, y)) numberOfNeighbours++;
+		if (this.isAliveNeighboursByCoors(x + 1, y + 1)) numberOfNeighbours++;
+
+		return numberOfNeighbours;
+	}
+
+	private isCellMatrixValidByCoors = (x: number, y: number) => {
+		return x >= 0 && x <= this.lastXIndex && y >= 0 && y <= this.lastYIndex;
+	};
+
+	private isAliveNeighboursByCoors = (x: number, y: number) => {
+		return this.isCellMatrixValidByCoors(x, y) && this.cellMatrix[x][y].getStatus() === Alive;
+	};
 }
 
+export { World };
 export default World;
--- src/tests/world.test.ts
+++ src/tests/world.test.ts
@@ -1,50 +1,8 @@
 import { Cell, CellStatus } from '../core/cell';
+import { World } from '../core/world';
 
 const Alive = CellStatus.alive;
 const Dead = CellStatus.dead;
-
-class World {
-	private readonly lastYIndex: number;
-	private readonly lastXIndex: number;
-
-	constructor(private cellMatrix: Cell[][]) {
-		this.cellMatrix = cellMatrix;
-		this.lastYIndex = cellMatrix.length - 1;
-		this.lastXIndex = cellMatrix.length - 1;
-	}
-
-	getCellMatrix = () => this.cellMatrix;
-
-	calculateNeighboursFromCoors(x: number, y: number) {
-		let numberOfNeighbours = 0;
-
-		/**
-		 * [x-1 & y-1] [ x  & y-1] [x+1 & y-1]
-		 * [x-1 &  y ] [   x y   ] [x+1 &  y ]
-		 * [x-1 & y+1] [ x  & y+1] [x+1 & y+1]
-		 */
-		if (this.isAliveNeighboursByCoors(x - 1, y - 1)) numberOfNeighbours++;
-		if (this.isAliveNeighboursByCoors(x - 1, y)) numberOfNeighbours++;
-		if (this.isAliveNeighboursByCoors(x - 1, y + 1)) numberOfNeighbours++;
-
-		if (this.isAliveNeighboursByCoors(x, y - 1)) numberOfNeighbours++;
-		if (this.isAliveNeighboursByCoors(x, y + 1)) numberOfNeighbours++;
-
-		if (this.isAliveNeighboursByCoors(x + 1, y - 1)) numberOfNeighbours++;
-		if (this.isAliveNeighboursByCoors(x + 1, y)) numberOfNeighbours++;
-		if (this.isAliveNeighboursByCoors(x + 1, y + 1)) numberOfNeighbours++;
-
-		return numberOfNeighbours;
-	}
-
-	private isCellMatrixValidByCoors = (x: number, y: number) => {
-		return x >= 0 && x <= this.lastXIndex && y >= 0 && y <= this.lastYIndex;
-	};
-
-	private isAliveNeighboursByCoors = (x: number, y: number) => {
-		return this.isCellMatrixValidByCoors(x, y) && this.cellMatrix[x][y].getStatus() === Alive;
-	};
-}
 
 describe('The World', () => {
 	it('creates a cell matrix for a given cell status', () => {
